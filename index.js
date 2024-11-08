fc6259993b502049be9a61ddf4bcfa7905cdc8ef
Rahul Gupta <rahul1990gupta@gmail.com>
2024-11-08 14:54:49 +0530

    refactors the code to use async/await

--- index.js
+++ index.js
@@ -27,18 +27,18 @@ let GameBoard = () =>{
     return{board, move, displayBoard}
 }
 
+GameState = {
+    PLAYING: "PLAYING",
+    WON: "WON",
+    DRAW: "DRAW"
+}
+
+
 let Game = (cs) => {
     gameBoard = GameBoard();
     currentPlayer = "O";
-    
-    GameState = {
-        PLAYING: "PLAYING",
-        WON: "WON",
-        DRAW: "DRAW"
-    }
 
     state = GameState.PLAYING;
-    
 
     function switchPlayer(){
         if (currentPlayer =="O"){
@@ -57,38 +57,41 @@ let Game = (cs) => {
         return true;
     }
 
-    function checkWin(){
-        // horizontal 
-        let b = gameBoard.board 
-        for (let i=0; i <3;i++){
-            if ((b[i][0] == b[i][1]) && (b[i][1] == b[i][2]) && (b[i][0] != " ")){
-                return b[i][0]
+    function checkWin() {
+        const bd = gameBoard.board;
+        const winningCombos = [
+            // Horizontal
+            [[0, 0], [0, 1], [0, 2]],
+            [[1, 0], [1, 1], [1, 2]],
+            [[2, 0], [2, 1], [2, 2]],
+            // Vertical
+            [[0, 0], [1, 0], [2, 0]],
+            [[0, 1], [1, 1], [2, 1]],
+            [[0, 2], [1, 2], [2, 2]],
+            // Diagonal
+            [[0, 0], [1, 1], [2, 2]],
+            [[0, 2], [1, 1], [2, 0]],
+        ];
+    
+        for (const combo of winningCombos) {
+            const [a, b, c] = combo;
+            if (bd[a[0]][a[1]] === bd[b[0]][b[1]] && bd[b[0]][b[1]] === bd[c[0]][c[1]] && bd[a[0]][a[1]] !== " ") {
+                return bd[a[0]][a[1]];
             }
         }
-        
-        // vertical 
-        for (let j=0; j <3;j++){
-            if ((b[0][j] == b[1][j]) && (b[1][j] == b[2][j]) &&  (b[0][j] != " ")){
-                return b[0][j];
-            }
-        }
-        
-        // crosses 
-        if ((b[0][0] == b[1][1]) && (b[1][1] == b[2][2]) && (b[0][0] != " ")) return b[0][0];
-
-        if ((b[0][2] == b[1][1]) && (b[1][1] == b[2][0]) && (b[2][0] != " ")) return b[0][2];
-        
+    
         return " ";
     }
-
-    function play(){
-        cs.readInput(currentPlayer, state, (row, col) => {
-            if (state == GameState.DRAW || state == GameState.WON) return state;
-
-            let status = gameBoard.move(currentPlayer, row, col)
-
-            gameBoard.displayBoard()
     
+
+    async function play(){
+        while (state == GameState.PLAYING) {
+            const {row, col} = await cs.readInput(currentPlayer);
+    
+            let moveStatus = gameBoard.move(currentPlayer, row, col);
+    
+            gameBoard.displayBoard()
+        
             winner = checkWin();
             if (winner == "O" || winner == "X"){
                 state = GameState.WON
@@ -99,78 +102,71 @@ let Game = (cs) => {
                 console.log("Game is Draw.")
             }
             else {
-                state = GameState.PLAYING
                 // if last move was legal
-                if (status){
-                    switchPlayer();
-                }
-                play();
-            }
-        
-            return state;
-        })
-
+                if (moveStatus) switchPlayer();
+            }        
+        }
     }
     return {gameBoard, play};
 }
 
 ConsoleScreen = () => {
-    function readInput(currentPlayer, state, callback){
+    function readInput(currentPlayer){
         console.log("Please enter the cell number for player (1-9):", currentPlayer)
-        process.stdin.resume();
-        process.stdin.setEncoding("utf8");
 
-        process.stdin.once("data", function (input) {
-            // Trim the input to remove any trailing newline or whitespace
-            ix = parseInt(input.trim()) -1;
-            
-            row = Math.floor(ix / 3);
-            col = ix % 3;
+        return new Promise((resolve) => {
+            process.stdin.resume();
+            process.stdin.setEncoding("utf8");
 
-            // To end the input after receiving one line
-            process.stdin.pause();
-            
-            callback(row, col);
+            process.stdin.once("data", function (input) {
+                // Trim the input to remove any trailing newline or whitespace
+                ix = parseInt(input.trim()) -1;
+                row = Math.floor(ix / 3);
+                col = ix % 3;
+                process.stdin.pause();
 
-        });
-
+                resolve({row, col});
+            });
+        })
     }
     return {readInput};
 }
 
+
 DOMWindow = () => {
-    function readInput(currentPlayer, state, callback){
+    function readInput(currentPlayer) {
         document.getElementById("player").innerText = currentPlayer;
 
-        result = document.getElementById("result");
-        cells= document.getElementsByClassName("cell");
-        for (let i=0; i <9; i++){
-            cells[i].addEventListener("click", (event) => {
-                
-                if(state == GameState.DRAW || state== GameState.WON) return;
-    
+        return new Promise((resolve) => {
+            const cells = document.getElementsByClassName("cell");
+
+            function clickHandler(event) {
+                const ix = event.target.dataset.key - 1;
+                const row = Math.floor(ix / 3);
+                const col = ix % 3;
+
+                // Update the cell's text with the current player's symbol
                 event.target.innerText = currentPlayer;
-                ix = event.target.dataset.key - 1;
-                row = Math.floor(ix / 3);
-                col = ix % 3;
-                state  = callback(row, col);
 
-                if (state == GameState.DRAW){
-                    result.innerText = "Game Draw";
+                // Clean up event listeners on all cells
+                for (let i = 0; i < cells.length; i++) {
+                    cells[i].removeEventListener("click", clickHandler);
                 }
-                else if( state == GameState.WON) {
-                    result.innerText = currentPlayer + " Won!"
-                }
-                else {
-                   
-                }
-            });
-        }
+
+                resolve({ row, col });
+            }
+
+            // Add click event listeners to each cell
+            for (let i = 0; i < cells.length; i++) {
+                cells[i].addEventListener("click", clickHandler);
+            }
+        });
     }
-    return {readInput}
-}
 
- c = ConsoleScreen();
+    return { readInput };
+};
 
-// c  = DOMWindow()
+//  c = ConsoleScreen();
+
+ c  = DOMWindow()
 Game(c).play()
